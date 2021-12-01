11564201b495c01ab5261ef626396b74814d5d52
doctone <samjojames@gmail.com>
2021-12-01 15:34:05 +0000

    added DateTime calculations

--- Program.cs
+++ Program.cs
@@ -6,20 +6,42 @@ namespace HelloWorld
     {
         static void Main()
         {
-            WelcomeMessage();
-            Operation();
-            OfferRepeat();
+            string choice = WelcomeMessage();
+            if (choice == "1")
+            {
+                NumOperation();
+                OfferRepeat(1);
+            } else
+            {
+                DateOperation();
+                OfferRepeat(2);
+            }
+
         }
 
 
 
-        static void WelcomeMessage()
+        static string WelcomeMessage()
         {
             Console.WriteLine("=== Welcome to Calculator Land ===");
+            Console.WriteLine("Which calculator would you like to use?");
+            Console.WriteLine("1. Numbers");
+            string calcChoice = GetInput("2. Dates \n");
+            
+            return calcChoice;
         }
 
 
-        static void Operation()
+        static void DateOperation()
+        {
+            string dateInput = GetInput("Please enter a date");
+            DateTime date = DateTime.Parse(dateInput);
+            int toAdd = int.Parse(GetInput("Enter the number of days you wish to add: "));
+            Console.WriteLine($"Your answer is {date.AddDays(toAdd)}");
+
+        }
+
+        static void NumOperation()
         {
             string operation = GetInput("Choose an operation : + - * /");
 
@@ -33,7 +55,7 @@ namespace HelloWorld
                 nums[i] = int.Parse(Console.ReadLine());
 
             }
-            int answer = nums[0];
+            double answer = nums[0];
             for (int i = 1; i < nums.Length; i++)
             {
                 if (operation == "+")
@@ -53,7 +75,8 @@ namespace HelloWorld
                     answer *= nums[i];
                 }
             }
-            Console.WriteLine(answer);
+            string finalAnswer = string.Format("The answer your looking for is {0}", answer);
+            Console.WriteLine(finalAnswer);
         }
         static int AskForNum(string message)
         {
@@ -72,13 +95,22 @@ namespace HelloWorld
             string input = Console.ReadLine();
             return input;
         }
-        static void OfferRepeat()
+        static void OfferRepeat(int option)
         {
             string answer = GetInput("Would you like to do another operation? Y/N: ");
-            if (answer == "Y")
+            if (answer == "Y" || answer == "y")
             {
-                Calculator.Operation();
-                Calculator.OfferRepeat();
+                if (option == 1)
+                {
+                    Calculator.NumOperation();
+                    Calculator.OfferRepeat(1);
+                }
+                else
+                {
+                    Calculator.DateOperation();
+                    Calculator.OfferRepeat(2);
+                }
+    
             }
             else
             {
